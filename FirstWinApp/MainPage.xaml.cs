5fbd017719ee02ddadfd1b5d57f1b27e07ecdf8e
jerryczhang <jerrychzhang@gmail.com>
2020-06-15 09:47:59 -0700

    initial commit

---
+++ FirstWinApp/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FirstWinApp
+{
+    public sealed partial class MainPage : Page
+    {
+        Random rand;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            rand = new Random();
+        }
+
+        private byte[] GetRandomBytes(int n)
+        {
+            var randomBytes = new byte[n];
+            rand.NextBytes(randomBytes);
+            return randomBytes;
+        }
+
+        private void SayHelloButton_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] rgb = GetRandomBytes(3);
+
+            var randomColorBrush = new SolidColorBrush(Color.FromArgb(255, rgb[0], rgb[1], rgb[2]));
+
+            textBox1.BorderBrush = randomColorBrush;
+            textBox1.Foreground = randomColorBrush;
+        }
+    }
+}
